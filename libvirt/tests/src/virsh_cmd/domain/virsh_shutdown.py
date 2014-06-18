6ef77ac73465f6d02c36e395c900732e69c25f86
Kyla Zhang <weizhan@redhat.com>
2014-06-18 22:52:20 +0800

    Modify for remove agent in reboot shutdown undefine

--- libvirt/tests/src/virsh_cmd/domain/virsh_shutdown.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_shutdown.py
@@ -1,10 +1,5 @@
 from autotest.client.shared import error
-from virttest import remote
-from virttest import libvirt_vm
-from virttest import virsh
-from virttest import utils_libvirtd
-from virttest.libvirt_xml import vm_xml
-from provider import libvirt_version
+from virttest import remote, libvirt_vm, virsh, utils_libvirtd
 
 
 def run(test, params, env):
@@ -22,65 +17,31 @@ def run(test, params, env):
 
     vm_name = params.get("main_vm")
     vm = env.get_vm(vm_name)
+
+    domid = vm.get_id()
+    domuuid = vm.get_uuid()
+
     vm_ref = params.get("shutdown_vm_ref")
-    status_error = ("yes" == params.get("status_error"))
-    agent = ("yes" == params.get("shutdown_agent", "no"))
-    mode = params.get("shutdown_mode", "")
-    pre_domian_status = params.get("reboot_pre_domian_status", "running")
     libvirtd = params.get("libvirtd", "on")
-    xml_backup = vm_xml.VMXML.new_from_inactive_dumpxml(vm_name)
 
-    # Libvirt acl test related params
-    uri = params.get("virsh_uri")
-    unprivileged_user = params.get('unprivileged_user')
-    if unprivileged_user:
-        if unprivileged_user.count('EXAMPLE'):
-            unprivileged_user = 'testacl'
-
-    if not libvirt_version.version_compare(1, 1, 1):
-        if params.get('setup_libvirt_polkit') == 'yes':
-            raise error.TestNAError("API acl test not supported in current"
-                                    + " libvirt version.")
+    # run test case
+    if vm_ref == "id":
+        vm_ref = domid
+    elif vm_ref == "hex_id":
+        vm_ref = hex(int(domid))
+    elif vm_ref.find("invalid") != -1:
+        vm_ref = params.get(vm_ref)
+    elif vm_ref == "name":
+        vm_ref = "%s %s" % (vm_name, params.get("shutdown_extra"))
+    elif vm_ref == "uuid":
+        vm_ref = domuuid
 
     try:
-
-        # Add or remove qemu-agent from guest before test
-        if agent:
-            vm_xml.VMXML.set_agent_channel(vm_name)
-        else:
-            vm_xml.VMXML.remove_agent_channel(vm_name)
-        virsh.start(vm_name)
-        guest_session = vm.wait_for_login()
-        if agent:
-            guest_session.cmd("service qemu-guest-agent start")
-            stat_ps = guest_session.cmd_status("ps aux |grep [q]emu-ga")
-            guest_session.close()
-            if stat_ps:
-                raise error.TestError("Fail to start qemu-guest-agent!")
-        if pre_domian_status == "shutoff":
-            virsh.destroy(vm_name)
-        domid = vm.get_id()
-        domuuid = vm.get_uuid()
-        # run test case
-        if vm_ref == "id":
-            vm_ref = domid
-        elif vm_ref == "hex_id":
-            vm_ref = hex(int(domid))
-        elif vm_ref.find("invalid") != -1:
-            vm_ref = params.get(vm_ref)
-        elif vm_ref == "name":
-            vm_ref = "%s %s" % (vm_name, params.get("shutdown_extra"))
-        elif vm_ref == "uuid":
-            vm_ref = domuuid
-
         if libvirtd == "off":
             utils_libvirtd.libvirtd_stop()
 
         if vm_ref != "remote":
-            status = virsh.shutdown(vm_ref, mode,
-                                    unprivileged_user=unprivileged_user,
-                                    uri=uri, debug=True,
-                                    ignore_status=True).exit_status
+            status = virsh.shutdown(vm_ref, ignore_status=True).exit_status
         else:
             remote_ip = params.get("remote_ip", "REMOTE.EXAMPLE.COM")
             remote_pwd = params.get("remote_pwd", None)
@@ -94,8 +55,7 @@ def run(test, params, env):
                 session = remote.remote_login("ssh", remote_ip, "22", "root",
                                               remote_pwd, "#")
                 session.cmd_output('LANG=C')
-                command = ("virsh -c %s shutdown %s %s"
-                           % (remote_uri, vm_name, mode))
+                command = "virsh -c %s shutdown %s" % (remote_uri, vm_name)
                 status = session.cmd_status(command, internal_timeout=5)
                 session.close()
             except error.CmdError:
@@ -106,13 +66,13 @@ def run(test, params, env):
             utils_libvirtd.libvirtd_start()
 
         # check status_error
-        if status_error:
-            if not status:
+        status_error = params.get("status_error")
+        if status_error == "yes":
+            if status == 0:
                 raise error.TestFail("Run successfully with wrong command!")
-        else:
-            if status:
+        elif status_error == "no":
+            if status != 0:
                 raise error.TestFail("Run failed with right command")
     finally:
-        xml_backup.sync()
         if vm.state() == "paused":
             vm.resume()
--- libvirt/tests/src/virsh_cmd/domain/virsh_undefine.py
+++ libvirt/tests/src/virsh_cmd/domain/virsh_undefine.py
@@ -1,16 +1,7 @@
 import os
 import logging
 from autotest.client.shared import error
-from autotest.client import utils
-from virttest import libvirt_vm
-from virttest import virsh
-from virttest import remote
-from virttest import utils_libvirtd
-from virttest import aexpect
-from virttest import libvirt_storage
-from virttest.libvirt_xml import vm_xml
-from virttest.utils_test import libvirt as utlv
-from provider import libvirt_version
+from virttest import libvirt_vm, virsh, remote, utils_libvirtd, aexpect
 
 
 def run(test, params, env):
@@ -28,46 +19,23 @@ def run(test, params, env):
 
     vm_ref = params.get("undefine_vm_ref", "vm_name")
     extra = params.get("undefine_extra", "")
-    option = params.get("undefine_option", "")
     libvirtd_state = params.get("libvirtd", "on")
-    status_error = ("yes" == params.get("status_error", "no"))
-    undefine_twice = ("yes" == params.get("undefine_twice", 'no'))
+    status_error = params.get("status_error")
+    undefine_twice = params.get("undefine_twice", 'no')
     local_ip = params.get("local_ip", "LOCAL.EXAMPLE.COM")
     remote_ip = params.get("remote_ip", "REMOTE.EXAMPLE.COM")
+    xml_file = os.path.join(test.tmpdir, 'tmp.xml')
     remote_user = params.get("remote_user", "user")
     remote_pwd = params.get("remote_pwd", "password")
     remote_prompt = params.get("remote_prompt", "#")
-    pool_type = params.get("pool_type")
-    pool_name = params.get("pool_name", "test")
-    pool_target = params.get("pool_target")
-    volume_size = params.get("volume_size", "1G")
-    vol_name = params.get("vol_name", "test_vol")
-    emulated_img = params.get("emulated_img", "emulated_img")
-    emulated_size = "%sG" % (int(volume_size[:-1]) + 1)
-    disk_target = params.get("disk_target", "vdb")
-    wipe_data = "yes" == params.get("wipe_data", "no")
-    if wipe_data:
-        option += " --wipe-storage"
 
-    vm_name = params.get("main_vm", "virt-tests-vm1")
+    vm_name = params.get("main_vm")
     vm = env.get_vm(vm_name)
     vm_id = vm.get_id()
     vm_uuid = vm.get_uuid()
 
-    # polkit acl related params
-    uri = params.get("virsh_uri")
-    unprivileged_user = params.get('unprivileged_user')
-    if unprivileged_user:
-        if unprivileged_user.count('EXAMPLE'):
-            unprivileged_user = 'testacl'
-
-    if not libvirt_version.version_compare(1, 1, 1):
-        if params.get('setup_libvirt_polkit') == 'yes':
-            raise error.TestNAError("API acl test not supported in current"
-                                    + " libvirt version.")
-
     # Back up xml file.Xen host has no guest xml file to define a guset.
-    backup_xml = vm_xml.VMXML.new_from_inactive_dumpxml(vm_name)
+    virsh.dumpxml(vm_name, extra="--inactive", to_file=xml_file)
 
     # Confirm how to reference a VM.
     if vm_ref == "vm_name":
@@ -81,161 +49,74 @@ def run(test, params, env):
     elif vm_ref.find("invalid") != -1:
         vm_ref = params.get(vm_ref)
 
-    volume = None
-    pvtest = None
-    status3 = None
+    # Turn libvirtd into certain state.
+    if libvirtd_state == "off":
+        utils_libvirtd.libvirtd_stop()
 
-    try:
-        save_file = "/var/lib/libvirt/qemu/save/%s.save" % vm_name
-        if option.count("managedsave") and vm.is_alive():
-            virsh.managedsave(vm_name)
+    # Test virsh undefine command.
+    if vm_ref != "remote":
+        vm_ref = "%s %s" % (vm_ref, extra)
+        cmdresult = virsh.undefine(vm_ref,
+                                   ignore_status=True, debug=True)
+        status = cmdresult.exit_status
+        if status:
+            logging.debug("Error status, command output: %s", cmdresult.stdout)
+        if undefine_twice == "yes":
+            status2 = virsh.undefine(vm_ref,
+                                     ignore_status=True).exit_status
+    else:
+        if remote_ip.count("EXAMPLE.COM") or local_ip.count("EXAMPLE.COM"):
+            raise error.TestNAError("remote_ip and/or local_ip parameters not"
+                                    " changed from default values")
+        try:
+            uri = libvirt_vm.complete_uri(local_ip)
+            session = remote.remote_login("ssh", remote_ip, "22", remote_user,
+                                          remote_pwd, remote_prompt)
+            cmd_undefine = "virsh -c %s undefine %s" % (uri, vm_name)
+            status, output = session.cmd_status_output(cmd_undefine)
+            logging.info("Undefine output: %s", output)
+        except (error.CmdError, remote.LoginError, aexpect.ShellError), detail:
+            logging.error("Detail: %s", detail)
+            status = 1
 
-        snp_list = virsh.snapshot_list(vm_name)
-        if option.count("snapshot"):
-            snp_file_list = []
-            if not len(snp_list):
-                virsh.snapshot_create(vm_name)
-                logging.debug("Create a snapshot for test!")
-            else:
-                # Backup snapshots for domain
-                for snp_item in snp_list:
-                    tmp_file = os.path.join(test.tmpdir, snp_item+".xml")
-                    virsh.snapshot_dumpxml(vm_name, snp_item, to_file=tmp_file)
-                    snp_file_list.append(tmp_file)
-        else:
-            if len(snp_list):
-                raise error.TestNAError("This domain has snapshot(s), "
-                                        "cannot be undefined!")
-        if option.count("remove-all-storage"):
-            pvtest = utlv.PoolVolumeTest(test, params)
-            pvtest.pre_pool(pool_name, pool_type, pool_target, emulated_img,
-                            emulated_size)
-            new_pool = libvirt_storage.PoolVolume(pool_name)
-            if not new_pool.create_volume(vol_name, volume_size):
-                raise error.TestFail("Create volume %s failed." % vol_name)
-            volumes = new_pool.list_volumes()
-            volume = volumes[vol_name]
-            virsh.attach_disk(vm_name, volume, disk_target, "--config")
+    # Recover libvirtd state.
+    if libvirtd_state == "off":
+        utils_libvirtd.libvirtd_start()
 
-        # Turn libvirtd into certain state.
-        if libvirtd_state == "off":
-            utils_libvirtd.libvirtd_stop()
+    # Shutdown VM.
+    if virsh.domain_exists(vm.name):
+        try:
+            if vm.is_alive():
+                vm.destroy()
+        except error.CmdError, detail:
+            logging.error("Detail: %s", detail)
 
-        # Test virsh undefine command.
-        output = ""
-        if vm_ref != "remote":
-            vm_ref = "%s %s" % (vm_ref, extra)
-            cmdresult = virsh.undefine(vm_ref, option,
-                                       unprivileged_user=unprivileged_user,
-                                       uri=uri,
-                                       ignore_status=True, debug=True)
-            status = cmdresult.exit_status
-            output = cmdresult.stdout.strip()
-            if status:
-                logging.debug("Error status, command output: %s",
-                              cmdresult.stderr.strip())
-            if undefine_twice:
-                status2 = virsh.undefine(vm_ref,
-                                         ignore_status=True).exit_status
-        else:
-            if remote_ip.count("EXAMPLE.COM") or local_ip.count("EXAMPLE.COM"):
-                raise error.TestNAError("remote_ip and/or local_ip parameters"
-                                        " not changed from default values")
-            try:
-                uri = libvirt_vm.complete_uri(local_ip)
-                session = remote.remote_login("ssh", remote_ip, "22",
-                                              remote_user, remote_pwd,
-                                              remote_prompt)
-                cmd_undefine = "virsh -c %s undefine %s" % (uri, vm_name)
-                status, output = session.cmd_status_output(cmd_undefine)
-                logging.info("Undefine output: %s", output)
-            except (error.CmdError, remote.LoginError, aexpect.ShellError), de:
-                logging.error("Detail: %s", de)
-                status = 1
+    # Check if VM exists.
+    vm_exist = virsh.domain_exists(vm.name)
 
-        # Recover libvirtd state.
-        if libvirtd_state == "off":
-            utils_libvirtd.libvirtd_start()
+    # Check if xml file exists.
+    xml_exist = False
+    if (os.path.exists("/etc/libvirt/qemu/%s.xml" % vm_name) or
+            os.path.exists("/etc/xen/%s" % vm_name)):
+        xml_exist = True
 
-        # Shutdown VM.
-        if virsh.domain_exists(vm.name):
-            try:
-                if vm.is_alive():
-                    vm.destroy(gracefully=False)
-            except error.CmdError, detail:
-                logging.error("Detail: %s", detail)
-
-        # Check if VM exists.
-        vm_exist = virsh.domain_exists(vm_name)
-
-        # Check if xml file exists.
-        xml_exist = False
-        if os.path.exists("/etc/libvirt/qemu/%s.xml" % vm_name) or\
-           os.path.exists("/etc/xen/%s" % vm_name):
-            xml_exist = True
-
-        # Check if save file exists if use --managed-save
-        save_exist = False
-        if os.path.exists(save_file):
-            save_exist = True
-
-        # Check if save file exists if use --managed-save
-        volume_exist = False
-        if volume and os.path.exists(volume):
-            volume_exist = True
-
-        # Test define with acl control and recover domain.
-        if params.get('setup_libvirt_polkit') == 'yes':
-            if virsh.domain_exists(vm.name):
-                virsh.undefine(vm_ref, ignore_status=True)
-            cmd = "chmod 666 %s" % backup_xml.xml
-            utils.run(cmd, ignore_status=False)
-            s_define = virsh.define(backup_xml.xml,
-                                    unprivileged_user=unprivileged_user,
-                                    uri=uri, ignore_status=True, debug=True)
-            status3 = s_define.exit_status
-
-    finally:
-        # Recover main VM.
-        backup_xml.sync()
-
-        # Clean up pool
-        if pvtest:
-            pvtest.cleanup_pool(pool_name, pool_type,
-                                pool_target, emulated_img)
-        # Recover VM snapshots.
-        if option.count("snapshot"):
-            logging.debug("Recover snapshots for domain!")
-            for file_item in snp_file_list:
-                virsh.snapshot_create(vm_name, file_item)
+    # Recover main VM.
+    if not virsh.domain_exists(vm.name):
+        s_define = virsh.define(xml_file)
+        if s_define is not True or not virsh.domain_exists(vm.name):
+            logging.error("Failed to define %s.", vm.name)
 
     # Check results.
-    if status_error:
-        if not status:
+    if status_error == 'yes':
+        if status == 0:
             raise error.TestFail("virsh undefine return unexpected result.")
-        if params.get('setup_libvirt_polkit') == 'yes':
-            if status3 == 0:
-                raise error.TestFail("virsh define with false acl permission" +
-                                     " should failed.")
     else:
-        if status:
+        if status != 0:
             raise error.TestFail("virsh undefine failed.")
-        if undefine_twice:
-            if not status2:
+        if undefine_twice == "yes":
+            if status2 == 0:
                 raise error.TestFail("Undefine the same VM twice succeeded.")
         if vm_exist:
             raise error.TestFail("VM still exists after undefine.")
         if xml_exist:
             raise error.TestFail("Xml file still exists after undefine.")
-        if option.count("managedsave") and save_exist:
-            raise error.TestFail("Save file still exists after undefine.")
-        if option.count("remove-all-storage") and volume_exist:
-            raise error.TestFail("Volume file '%s' still exists after"
-                                 " undefine." % volume)
-        if wipe_data and option.count("remove-all-storage"):
-            if not output.count("Wiping volume '%s'" % disk_target):
-                raise error.TestFail("Command didn't wipe volume storage!")
-        if params.get('setup_libvirt_polkit') == 'yes':
-            if status3:
-                raise error.TestFail("virsh define with right acl permission" +
-                                     " should succeeded")
